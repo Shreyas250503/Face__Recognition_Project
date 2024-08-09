91566f5e72de82f57cfb2218364fd02e2b547a06
Shreyas Kartikey <shreyaskartikey25@gmail.com>
2024-08-09 12:40:37 +0530

    Face and name data collection is done

--- add_faces.py
+++ add_faces.py
@@ -1,11 +1,15 @@
 import cv2
 import pickle
+import numpy as np
+import os
 
 video = cv2.VideoCapture(1)
 facedetect = cv2.CascadeClassifier("data/haarcascade_frontalface_default.xml")
 
 faces_data = []
 i = 0
+
+name = input("Enter Your Name: ")
 
 
 while True:
@@ -15,7 +19,7 @@ while True:
     for (x, y, w, h ) in faces:
         crop_img = frame[y:y +h, x:x + w, :]
         resized_img = cv2.resize(crop_img, (50, 50))
-        if len(faces_data) <= 100:
+        if len(faces_data) <= 100 and i%10 == 0:
             faces_data.append(resized_img)
         i = i + 1
         cv2.putText(frame, str(len(faces_data)), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 255), 1)
@@ -27,3 +31,30 @@ while True:
 
 video.release()
 cv2.destroyAllWindows()
+
+faces_data = np.asarray( faces_data)
+faces_data = faces_data.reshape( 100, -1)
+
+if "names.pkl" not in os.listdir('data/'):
+    names = 100 * [name]
+    with open ('data/names.pkl', 'wb') as f:
+        pickle.dump(names, f)
+
+else:
+    with open( 'data/ names.pkl', 'rb') as f:
+        names = pickle.load(f)
+    names =names + name*100
+    with open ('data/names.pkl', 'wb') as f:
+        pickle.dump(names, f)
+
+if "faces_data.pkl" not in os.listdir('data/'):
+    with open ('data/faces_data.pkl', 'wb') as f:
+        pickle.dump(faces_data, f)
+
+else:
+    with open( 'data/faces_data.pkl', 'rb') as f:
+        faces = pickle.load(f)
+    feaces = np.append( faces, faces_data, axis = 0)
+    with open ('data/faces_data.pkl', 'wb') as f:
+        pickle.dump(names, f)
+              
